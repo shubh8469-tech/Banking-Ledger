5cfba99ff93c0252392cdaa0b576b6df9ea32e27
Shubham Mishra <>
2026-08-13 00:11:20 +0530

    Port conflict resolved

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const connectToDB = require('./src/config/db');
 
 connectToDB();
 
-app.listen(3000, ()=>{
+app.listen(3001, ()=>{
     console.log("Server is running on port 3000");
 });
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -27,11 +27,11 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")){
-        return next();
+        return;// next();
     }
 
     this.password = await bcrypt.hash(this.password, 10);
-    return next();
+    return;// next();
 });
 
 userSchema.methods.comparePassword = async function(password){
